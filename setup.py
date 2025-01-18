5d1e254a6a04e9e84ff602ee8c9dfa482d08ec8a
Alessia Sarritzu <alesarritz@gmail.com>
2025-01-18 17:46:05 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup 
+
+setup(
+  name='ASCII Art Generator', 
+  version='1.0',
+  author='Alessia Sarritzu', 
+  license='MIT',
+  long_description=open('README.md').read()
+)
